d092ada0cc84cd2307aebf28785c56d265a3d465
Dreamwave7 <dima190179@gmail.com>
2023-06-29 19:21:23 +0300

    ф

--- database/db.py
+++ database/db.py
@@ -5,17 +5,6 @@ from sqlalchemy.orm import sessionmaker
 sql_key = "postgresql+psycopg2://dkwvkeys:aD0Tydu8H8DSKRQKPMEIAVSLcKK-a6CM@tyke.db.elephantsql.com/dkwvkeys"
 engine = create_engine(sql_key)
 session = sessionmaker(autocommit = False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-class Note(Base):
-    __tablename__ = "notes"
-    id = Column(Integer(), primary_key=True, index=True)
-    name = Column(String(100))
-    description = Column(String(200))
-    done = Column(Boolean(),default=False)
-
-
-Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = session()
