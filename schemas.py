9737484fb8d4e1cced6148d5d498a669b1c737bf
Dreamwave7 <dima190179@gmail.com>
2023-07-01 00:53:12 +0300

    d

---
+++ schemas.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from typing import List, Optional
+from pydantic import BaseModel, Field
+
+class TagModel(BaseModel):
+    name :str = Field(max_length=25)
+
+class TagResponse(TagModel):
+    id :int
+
+    class Config:
+        orm_mode = True
+
+class NoteBase(BaseModel):
+    title: str = Field(max_length=200)
+    description :str = Field(max_length=200)
+
+class NoteModel(NoteBase):
+    tags: List[int]
+
+class NoteUpdate(NoteModel):
+    done :bool
+
+class NoteStatusUpdate(BaseModel):
+    done:bool
+
+class NoteResponse(NoteBase):
+    id:int
+    created_at : datetime
+    tags: List[TagResponse]
+
+    class Config:
+        orm_mode = True
